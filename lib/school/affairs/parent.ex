fe98e34fdb54b504ad9a4b8bfd8d1dd87b9f41f5
foongef <foongef@gmail.com>
2018-09-25 12:36:13 +0800

    fix bugs

--- lib/school/affairs/parent.ex
+++ lib/school/affairs/parent.ex
@@ -2,41 +2,40 @@ defmodule School.Affairs.Parent do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "parent" do
-    field :addr1, :string
-    field :addr2, :string
-    field :addr3, :string
-    field :cname, :string
-    field :country, :string
-    field :district, :string
-    field :epaddr1, :string
-    field :epaddr2, :string
-    field :epaddr3, :string
-    field :epdistrict, :string
-    field :epname, :string
-    field :epposcod, :string
-    field :epstate, :string
-    field :hphone, :string
-    field :htel, :string
-    field :icno, :string
-    field :income, :string
-    field :inctaxno, :string
-    field :nacert, :string
-    field :name, :string
-    field :nation, :string
-    field :occup, :string
-    field :oldic, :string
-    field :otel, :string
-    field :poscod, :string
-    field :pstatus, :string
-    field :race, :string
-    field :refno, :string
-    field :relation, :string
-    field :religion, :string
-    field :state, :string
-    field :tanggn, :string
-    field :institution_id, :integer
+    field(:addr1, :string)
+    field(:addr2, :string)
+    field(:addr3, :string)
+    field(:cname, :string)
+    field(:country, :string)
+    field(:district, :string)
+    field(:epaddr1, :string)
+    field(:epaddr2, :string)
+    field(:epaddr3, :string)
+    field(:epdistrict, :string)
+    field(:epname, :string)
+    field(:epposcod, :string)
+    field(:epstate, :string)
+    field(:hphone, :string)
+    field(:htel, :string)
+    field(:icno, :string)
+    field(:income, :string)
+    field(:inctaxno, :string)
+    field(:nacert, :string)
+    field(:name, :string)
+    field(:nation, :string)
+    field(:occup, :string)
+    field(:oldic, :string)
+    field(:otel, :string)
+    field(:poscod, :string)
+    field(:pstatus, :string)
+    field(:race, :string)
+    field(:refno, :string)
+    field(:relation, :string)
+    field(:religion, :string)
+    field(:state, :string)
+    field(:tanggn, :string)
+    field(:institution_id, :integer)
 
     timestamps()
   end
@@ -44,7 +43,40 @@ defmodule School.Affairs.Parent do
   @doc false
   def changeset(parent, attrs) do
     parent
-    |> cast(attrs, [:institution_id, :icno, :name, :cname, :relation, :race, :religion, :nation, :country, :nacert, :pstatus, :tanggn, :oldic, :refno, :occup, :income, :addr1, :addr2, :addr3, :poscod, :district, :state, :htel, :otel, :hphone, :epname, :epaddr1, :epaddr2, :epaddr3, :epposcod, :epdistrict, :epstate, :inctaxno])
-    |> validate_required([:institution_id, :icno, :name, :cname, :relation, :race, :religion, :nation, :country, :nacert, :pstatus, :tanggn, :oldic, :refno, :occup, :income, :addr1, :addr2, :addr3, :poscod, :district, :state, :htel, :otel, :hphone, :epname, :epaddr1, :epaddr2, :epaddr3, :epposcod, :epdistrict, :epstate, :inctaxno])
+    |> cast(attrs, [
+      :institution_id,
+      :icno,
+      :name,
+      :cname,
+      :relation,
+      :race,
+      :religion,
+      :nation,
+      :country,
+      :nacert,
+      :pstatus,
+      :tanggn,
+      :oldic,
+      :refno,
+      :occup,
+      :income,
+      :addr1,
+      :addr2,
+      :addr3,
+      :poscod,
+      :district,
+      :state,
+      :htel,
+      :otel,
+      :hphone,
+      :epname,
+      :epaddr1,
+      :epaddr2,
+      :epaddr3,
+      :epposcod,
+      :epdistrict,
+      :epstate,
+      :inctaxno
+    ])
   end
 end
