a8a95446c54bafa229c5ae6fef2d0729011874e5
Damien Lee <yithanglee@gmail.com>
2018-10-22 10:44:31 +0800

    delete excess dup

--- priv/repo/migrations/20180930091356_add_inst_id_to_cocurriculum.exs
+++
@@ -1,9 +0,0 @@
-defmodule School.Repo.Migrations.AddInstIdToCocurriculum do
-  use Ecto.Migration
-
-  def change do
-alter table("cocurriculum") do
-      add(:institution_id, :integer)
-  end
-  end
-end
