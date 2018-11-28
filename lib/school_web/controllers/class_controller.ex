0f3e4667ee0430fd51b86ea49284b0babfc270a4
Damien Lee <yithanglee@gmail.com>
2018-11-28 10:53:36 +0800

    class

--- lib/school_web/controllers/class_controller.ex
+++ lib/school_web/controllers/class_controller.ex
@@ -76,7 +76,8 @@ defmodule SchoolWeb.ClassController do
             id: t.id,
             ic: t.ic,
             student_no: t.student_no,
-            phone: t.phone
+            phone: t.phone,
+            b_cert: t.b_cert
           }
         )
       )
@@ -99,10 +100,24 @@ defmodule SchoolWeb.ClassController do
   def reg_lib_student(student, lib_id, uri) do
     name = String.replace(student.name, " ", "+")
 
-    path =
-      "?scope=get_user_register_response&lib_id=#{lib_id}&name=#{name}&ic=#{student.ic}&phone=#{
+    ic =
+      if student.ic == nil do
+        student.b_cert
+      else
+        student.ic
+      end
+
+    phone =
+      if student.phone == nil do
+        "no_phone"
+      else
         student.phone
-      }&code=#{student.student_no}"
+      end
+
+    path =
+      "?scope=get_user_register_response&lib_id=#{lib_id}&name=#{name}&ic=#{ic}&phone=#{phone}&code=#{
+        student.student_no
+      }"
 
     IO.inspect(uri <> path)
     response = HTTPoison.get!(uri <> path, [{"Content-Type", "application/json"}]).body
