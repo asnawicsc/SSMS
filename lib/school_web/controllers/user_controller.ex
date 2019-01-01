0bf77a3e16eb488aaabc932da121165ff1a07b94
Your Nawi <asnawicsc@gmail.com>
2019-01-01 11:25:46 +0800

    update new

--- lib/school_web/controllers/user_controller.ex
+++ lib/school_web/controllers/user_controller.ex
@@ -109,12 +109,12 @@ defmodule SchoolWeb.UserController do
               %{id: 0, start_date: "Not set", end_date: "Not set"}
             end
 
-          conn
-          |> put_session(:user_id, user.id)
-          |> put_session(:semester_id, current_sem.id)
-          |> put_session(:institution_id, institution_id.id)
-          |> put_session(:style, user.styles)
-          |> redirect(to: page_path(conn, :admin_dashboard))
+          # conn
+          # |> put_session(:user_id, user.id)
+          # |> put_session(:semester_id, current_sem.id)
+          # |> put_session(:institution_id, institution_id.id)
+          # |> put_session(:style, user.styles)
+          # |> redirect(to: page_path(conn, :admin_dashboard))
         end
 
         access = Repo.get_by(Settings.UserAccess, user_id: user.id)
@@ -124,7 +124,7 @@ defmodule SchoolWeb.UserController do
             from(
               s in School.Affairs.Semester,
               where:
-                s.end_date > ^Timex.today() and s.start_date < ^Timex.today() and
+                s.end_date >= ^Timex.today() and s.start_date <= ^Timex.today() and
                   s.institution_id == ^access.institution_id
             )
           )
