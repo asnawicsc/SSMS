18b6fbca698a779ec1844d1581b0b3859e5bbb77
Your Nawi <asnawicsc@gmail.com>
2019-01-03 14:18:11 +0800

    update new

--- lib/school_web/channels/user_channel.ex
+++ lib/school_web/channels/user_channel.ex
@@ -1128,7 +1128,7 @@ defmodule SchoolWeb.UserChannel do
           where: s.class_id == ^class_id and s.semester_id == ^semester_id,
           select: %{
             id: r.id,
-            id_no: r.sudent_id,
+            id_no: r.student_no,
             chinese_name: r.chinese_name,
             name: r.name,
             sex: r.sex,
