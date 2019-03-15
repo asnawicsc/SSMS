2ab0a38666cb6722b8a269bda781d0efb88d7730
Your Nawi <asnawicsc@gmail.com>
2019-03-15 16:03:59 +0800

    update new

--- priv/repo/migrations/20190314100435_add_exam_m_name_to_marksheet_temp.exs
+++
@@ -1,9 +0,0 @@
-defmodule School.Repo.Migrations.AddExamMNameToMarksheetTemp do
-  use Ecto.Migration
-
-  def change do
-    alter table("mark_sheet_temp") do
-      add(:exam_name, :string)
-    end
-  end
-end
