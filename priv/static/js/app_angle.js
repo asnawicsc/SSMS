5a91f0aa45ea28160c5c0349d026c9293557dc67
Your Nawi <asnawicsc@gmail.com>
2019-03-27 17:51:01 +0800

    update

--- priv/static/js/app_angle.js
+++ priv/static/js/app_angle.js
@@ -3778,7 +3778,6 @@
             'paging': true, // Table pagination
             'ordering': true, // Column ordering
             'info': true, // Bottom left status text
-
             responsive: true,
             // Text translation options
             // Note the required keywords between underscores (e.g _MENU_)
@@ -3795,30 +3794,6 @@
                 }
             }
         });
-
-         $('#datatable99').DataTable({
-            'paging': true, // Table pagination
-            'ordering': true, // Column ordering
-            'info': true, // Bottom left status text
-            "pageLength": 100,
-            responsive: true,
-            // Text translation options
-            // Note the required keywords between underscores (e.g _MENU_)
-            oLanguage: {
-                sSearch: '<em class="fas fa-search"></em>',
-                sLengthMenu: '_MENU_ records per page',
-                info: 'Showing page _PAGE_ of _PAGES_',
-                zeroRecords: 'Nothing found - sorry',
-                infoEmpty: 'No records available',
-                infoFiltered: '(filtered from _MAX_ total records)',
-                oPaginate: {
-                    sNext: '<em class="fa fa-caret-right"></em>',
-                    sPrevious: '<em class="fa fa-caret-left"></em>'
-                }
-            }
-        });
-
-
 
         // Filter
 
@@ -3856,6 +3831,7 @@
             'paging': true, // Table pagination
             'ordering': true, // Column ordering
             'info': true, // Bottom left status text
+              "pageLength": 100,
             responsive: true,
             // Text translation options
             // Note the required keywords between underscores (e.g _MENU_)
