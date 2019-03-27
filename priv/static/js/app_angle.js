a07bccb80e81053d3a49f54c1d1e39d65cdb1d03
Your Nawi <asnawicsc@gmail.com>
2019-03-27 17:46:44 +0800

    udate

--- priv/static/js/app_angle.js
+++ priv/static/js/app_angle.js
@@ -3778,6 +3778,7 @@
             'paging': true, // Table pagination
             'ordering': true, // Column ordering
             'info': true, // Bottom left status text
+
             responsive: true,
             // Text translation options
             // Note the required keywords between underscores (e.g _MENU_)
@@ -3794,6 +3795,29 @@
                 }
             }
         });
+
+         $('#datatable99').DataTable({
+            'paging': true, // Table pagination
+            'ordering': true, // Column ordering
+            'info': true, // Bottom left status text
+            "pageLength": 100,
+            responsive: true,
+            // Text translation options
+            // Note the required keywords between underscores (e.g _MENU_)
+            oLanguage: {
+                sSearch: '<em class="fas fa-search"></em>',
+                sLengthMenu: '_MENU_ records per page',
+                info: 'Showing page _PAGE_ of _PAGES_',
+                zeroRecords: 'Nothing found - sorry',
+                infoEmpty: 'No records available',
+                infoFiltered: '(filtered from _MAX_ total records)',
+                oPaginate: {
+                    sNext: '<em class="fa fa-caret-right"></em>',
+                    sPrevious: '<em class="fa fa-caret-left"></em>'
+                }
+            }
+        });
+
 
 
         // Filter
